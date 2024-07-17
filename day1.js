50ef1d70f57926e180a627bf0136367c9d415bde
nitika <nitikasheoran5@gmail.com>
2024-07-18 00:19:57 +0530

    day 1 tasks completed

---
+++ day1.js
@@ -0,0 +1,30 @@
+// Activity 1
+var num = 1;
+console.log(" var number: ",num);
+let str = "hello,this is day 1 of 30 days of js challenge";
+console.log("let string: ",str);
+
+// Activity 2
+const bool = true;
+console.log("const boolean: ",bool);
+
+// Activity 3
+let n1 = 2;
+let s1 = "string";
+let b1 = true;
+let obj = {
+    "n1":2,
+    "str":"string",
+    "b1": true,
+};
+let arr = [1,2,3];
+console.log(typeof(n1),typeof(s1),typeof(b1),typeof(obj),typeof(arr));
+
+// Activity 4
+let initialVal = 1;
+initialVal = 2;
+console.log("initial value: ",initialVal);
+
+// Activity 5
+const val = "nitika";
+val = "sheoran";
