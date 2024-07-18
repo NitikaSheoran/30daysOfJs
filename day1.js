3fa2694e095ad35be5245745ea071644a4469ed2
NitikaSheoran <nitikasheoran5@gmail.com>
2024-07-19 02:16:35 +0530

    added comments

--- day1.js
+++ day1.js
@@ -1,30 +1,46 @@
-// Activity 1
+// Activity 1 --> Variable declaration
+// Task1  --> Declare a variable using var ,assign it a number, and log the value to console
 var num = 1;
-console.log(" var number: ",num);
-let str = "hello,this is day 1 of 30 days of js challenge";
-console.log("let string: ",str);
+console.log("var number: ",num);  //output == var number: 1
 
-// Activity 2
+// Task2  --> Declare a variable using let ,assign it a string, and log the value to console
+let str = "hello, this is day 1 of 30 days of js challenge";  
+console.log("let string: ",str);  //output == let string: hello,this is day 1 of 30 days of js challenge
+
+// Activity 2 --> Constant declaration
+// Task3  --> Declare a variable using const ,assign it a boolean value, and log the value to console
 const bool = true;
-console.log("const boolean: ",bool);
+console.log("const boolean: ",bool);  //output == const boolean: true
 
 // Activity 3
-let n1 = 2;
-let s1 = "string";
-let b1 = true;
-let obj = {
+// Task4  --> Create variables of different data types and log each variable's type using typeof operator
+let number1 = 2;
+let string1 = "string";
+let boolean1 = true;
+let object1 = {
     "n1":2,
     "str":"string",
     "b1": true,
 };
-let arr = [1,2,3];
-console.log(typeof(n1),typeof(s1),typeof(b1),typeof(obj),typeof(arr));
+let array1 = [1,2,3];
+let ud = null;
+console.log(" number type = ",typeof(number1),
+            "\n String type = ",typeof(string1),
+            "\n boolean type = ",typeof(boolean1),
+            "\n object type = ",typeof(object1),
+            "\n array type = ",typeof(array1),
+            "\n null type = ",typeof(ud));
 
 // Activity 4
+// Task5  --> Declare a variable using let,assign it an initial value, reassign a new value, log both the values to console
 let initialVal = 1;
+console.log("initial value: ",initialVal);  //output = initial value: 1
 initialVal = 2;
-console.log("initial value: ",initialVal);
+console.log("initial value: ",initialVal); //output = initial value: 2
 
 // Activity 5
+// Task6 --> try reassigning a variable declared using const and observe the error
 const val = "nitika";
-val = "sheoran";
+console.log("const val: ",val);  //output = const val: nitika
+// val = "sheoran";
+// console.log(val)  //error  TypeError: Assignment to constant variable
